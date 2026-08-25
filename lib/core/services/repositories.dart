0b35100580a9a3b82b7fee2207a068f42a9b164f
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-26 02:19:07 +0300

    refactor: unify repository contracts for Firebase-backed UI

--- lib/core/services/repositories.dart
+++ lib/core/services/repositories.dart
@@ -4,10 +4,15 @@ abstract interface class HomeRepository {
   List<HomeProfile> watchHomes();
   List<LocationArea> watchLocations();
   LocationArea? locationById(String id);
+  void upsertHome(HomeProfile home);
+  void deleteHome(String id);
+  void upsertLocation(LocationArea location);
+  void deleteLocation(String id);
 }
 
 abstract interface class AssetRepository {
   List<HomeAsset> watchAssets();
+  List<HomeAsset> archivedAssets();
   HomeAsset? assetById(String id);
   void addAsset(HomeAsset asset);
   void updateAsset(HomeAsset asset);
@@ -28,14 +33,39 @@ abstract interface class ReminderRepository {
 
 abstract interface class ProviderRepository {
   List<ProviderContact> watchProviders();
+  void upsertProvider(ProviderContact provider);
+  void deleteProvider(String id);
+}
+
+abstract interface class ServiceRepository {
+  List<ServicePlan> watchServices();
+  void upsertService(ServicePlan service);
+  void deleteService(String id);
+  void markServiceVisitCompleted(String id);
+}
+
+abstract interface class WarrantyRepository {
+  List<Warranty> watchWarranties();
+  void upsertWarranty(Warranty warranty);
+  void deleteWarranty(String id);
 }
 
 abstract interface class DocumentRepository {
   List<HomeDocument> watchDocuments();
+  void upsertDocument(HomeDocument document);
+  void deleteDocument(String id);
 }
 
 abstract interface class ExpenseRepository {
   List<Expense> watchExpenses();
+  void upsertExpense(Expense expense);
+  void deleteExpense(String id);
+}
+
+abstract interface class FamilyRepository {
+  List<FamilyMember> watchFamily();
+  void upsertFamilyMember(FamilyMember member);
+  void removeFamilyMember(String id);
 }
 
 abstract interface class ActivityRepository {
